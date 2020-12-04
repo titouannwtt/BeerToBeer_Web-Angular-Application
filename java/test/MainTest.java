faba024cc56f8d1fda33704b28125fc70b5aa0a2
CactusKipic <clement.azan@gmail.com>
2020-12-04 04:13:40 +0100

    Update ranking -> Mock response

--- java/test/MainTest.java
+++ java/test/MainTest.java
@@ -1,4 +1,4 @@
-import fr.esiea.ranking.Ranking;
+import fr.esiea.model.Ranking;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Array;
@@ -16,7 +16,7 @@ public class MainTest {
         HashMap<String, Integer> map = new HashMap<>();
     
         Arrays.stream(noms).forEach(c -> map.put(c, rand.nextInt(10000)));
-        map.entrySet().forEach(c -> System.out.println(c.getKey()+":"+c.getValue()));
+        map.forEach((key, value) -> System.out.println(key + ":" + value));
         Ranking rank = new Ranking("week");
         rank.makeRank(map);
         
