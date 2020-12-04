ed17e5b735e9bf1862dc6c9df01c7961cde1491f
Weaxell <thomascarpentier5@gmail.com>
2020-12-04 06:38:29 +0100

    Reorganisation du code pour que la compil en jar fonctionne

--- java/src/fr/esiea/RestApp.java
+++ java/src/fr/esiea/RestApp.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestApp
-{
+public class RestApp {
   public static void main(String[] args) {
     SpringApplication.run(RestApp.class, args);
   }
